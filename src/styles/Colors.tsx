76424dcba42d647edc74f7de0fda486661f8a9dd
moris96 <moriskhoudari@gmail.com>
2024-01-09 12:48:09 -0500

    dark mode context implemented

--- src/styles/Colors.tsx
+++ src/styles/Colors.tsx
@@ -1,3 +1,11 @@
-// export const myColors = {
-
-// }
+export const myColors = {
+    light: "#F1F2F3",
+    dark: "#17171C",
+    blue: "#4B5EFC",
+    btnGray: "#4E505F",
+    btnDark: "#2E2F38",
+    gray: "#747477",
+    black: "#00000",
+    white: "#FFFFF",
+    result: "#46D528",
+};
